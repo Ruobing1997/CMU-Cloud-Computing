2bb3f29a8da27c8eeddf527ab0a63df9eb4f5612
Ruobing1997 <80226058+Ruobing1997@users.noreply.github.com>
2022-02-01 23:35:57 -0500

    update task1

--- Project1/VM_Scaling_Project/python/task1/horizontal-scaling.py
+++ Project1/VM_Scaling_Project/python/task1/horizontal-scaling.py
@@ -1,14 +1,17 @@
 
+from datetime import datetime
+from anyio import current_time
 import boto3
 import botocore
 import os
+import pytz
 import requests
 import time
 import json
 import configparser
 import re
 from dateutil.parser import parse
-
+from tables import Description
 
 ########################################
 # Constants
@@ -46,11 +49,24 @@ def create_instance(ami, sg_name):
     :param sg_name: name of the security group to be attached to instance
     :return: instance object
     """
-    instance = None
+    # instance = None
 
-    # TODO: Create an EC2 instance
+    # Create an EC2 instance
     # Wait for the instance to enter the running state
     # Reload the instance attributes
+
+    # decalare ec2:
+    ec2 = boto3.resource('ec2')
+    instance = ec2.create_instances(
+        ImageId = ami,
+        InstanceType = INSTANCE_TYPE,
+        SecurityGroups = sg_name,
+        TagSpecifications = {
+            'Tags': [
+                TAGS
+            ]
+        }
+    )
 
     return instance
 
@@ -74,8 +90,8 @@ def initialize_test(lg_dns, first_web_service_dns):
             time.sleep(1)
             pass 
 
-    # TODO: return log File name
-    return ""
+    # return log File name
+    return get_test_id(response)
 
 
 def print_section(msg):
@@ -203,6 +219,48 @@ def get_test_start_time(lg_dns, log_name):
         start_time = dict(config.items('Test')).get('starttime', None)
     return parse(start_time)
 
+def create_security_group(group_name, group_description, sg_permissions):
+    ec2 = boto3.client('ec2')
+    response = ec2.describe_vpcs()
+    vpc_id = response.get('Vpcs', [{}])[0].get('VpcId', '')
+    try:
+        response = ec2.create_security_group(
+            GroupName = group_name,
+            Description = group_description,
+            TagSpecifications = {
+                'Tags' : TAGS
+            }
+        )
+        security_group_id = response['GroupId']
+        print('Security Group Created %s in vpc %s.' % (security_group_id, vpc_id))
+        data = ec2.authorize_security_group_ingress(
+            GroupId=security_group_id,
+            IpPermissions = sg_permissions
+        )
+        print('Ingress Successfully Set %s' % data)
+        return security_group_id
+    except botocore.exceptions.ClientError as e:
+        print(e)
+
+def update_current_time():
+    return datetime.now().astimezone(pytz.UTC)
+def parse_datetime_object(last_launch_time):
+    last_launch_datetime = datetime.strptime(last_launch_time.split("+")[0], "%Y-%m-%dT%H:%M:%S")
+    return last_launch_datetime
+def calculate_interval_sec(cur, prev):
+    duration = cur - prev
+    return duration.total_seconds()
+def check_running_instances():
+    ec2 = boto3.resource('ec2')
+    ls_running_instances = []
+    instances = ec2.instances.filter(
+        Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
+    for instance in instances:
+        ls_running_instances.append(instance.id)
+    return ls_running_instances
+def terminate_instances(ls_running_instances):
+    ec2 = boto3.resource('ec2')
+    ec2.instances.filter(InstanceIds=ls_running_instances).terminate()
 
 ########################################
 # Main routine
@@ -226,35 +284,57 @@ def main():
          }
     ]
 
-    # TODO: Create two separate security groups and obtain the group ids
-    sg1_id = None  # Security group for Load Generator instances
-    sg2_id = None  # Security group for Web Service instances
+    # Create two separate security groups and obtain the group ids
+    sg1_id = create_security_group(
+        "Load Generator HTTP Sec Group", 
+        "Permissions for load generator",
+        sg_permissions
+    )  # Security group for Load Generator instances
+    sg2_id = create_security_group(
+        "Web Service HTTP Sec Group", 
+        "Permissions for Web Service",
+        sg_permissions
+    )  # Security group for Web Service instances
 
     print_section('2 - create LG')
 
-    # TODO: Create Load Generator instance and obtain ID and DNS
-    lg = ''
-    lg_id = ''
-    lg_dns = ''
+    # Create Load Generator instance and obtain ID and DNS
+    ec2 = boto3.resource('ec2')
+    lg = create_instance(LOAD_GENERATOR_AMI, ec2.SecurityGroup(sg1_id).group_name)
+    lg.wait_until_running()
+    lg_id = lg.instance_id
+    # since the dns name is not available until the running state,
+    # so use wait_until_running
+    lg_dns = lg.public_dns_name
     print("Load Generator running: id={} dns={}".format(lg_id, lg_dns))
 
     print_section('3. Authenticate with the load generator')
     authenticate(lg_dns, SUBMISSION_PASSWORD, SUBMISSION_USERNAME)
 
-    # TODO: Create First Web Service Instance and obtain the DNS
-    web_service_dns = ''
+    # Create First Web Service Instance and obtain the DNS
+    web_service = create_instance(WEB_SERVICE_AMI, ec2.SecurityGroup(sg2_id).group_name)
+    web_service.wait_until_running()
+    web_service_dns = web_service.public_dns_name
 
     print_section('4. Submit the first WS instance DNS to LG, starting test.')
     log_name = initialize_test(lg_dns, web_service_dns)
     last_launch_time = get_test_start_time(lg_dns, log_name)
+    last_launch_datetime = parse_datetime_object(last_launch_time)
     while not is_test_complete(lg_dns, log_name):
-        # TODO: Check RPS and last launch time
-        # TODO: Add New Web Service Instance if Required
+        # Check RPS and last launch time
+        # Add New Web Service Instance if Required
         time.sleep(1)
-
+        current_RPS = get_rps(lg_dns, log_name)
+        current_time = update_current_time()
+        if (calculate_interval_sec(current_time, last_launch_datetime) > 100):
+            if (current_RPS < 50):
+                add_web_service_instance(lg_dns, ec2.SecurityGroup(sg2_id).group_name, log_name)
+                last_launch_datetime = current_time
+        current_time = update_current_time()
     print_section('End Test')
 
-    # TODO: Terminate Resources
+    # Terminate Resources
+    terminate_instances(check_running_instances())
 
 
 if __name__ == '__main__':
